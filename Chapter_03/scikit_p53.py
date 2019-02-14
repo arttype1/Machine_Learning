837fef1682445dfdfa4329ebff320b5684eca370
George Merrill <gm.webdev0@gmail.com>
2019-02-14 14:56:03 -0500

    Create scikit_p53.py

---
+++ Chapter_03/scikit_p53.py
@@ -0,0 +1,10 @@
+#code is from 'Python Maching Learning' by Raschka and Mirialili
+#################################################################################
+from sklearn import datasets
+import numpy as np
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+print('Class labels:', np.unique(y))
+
+
