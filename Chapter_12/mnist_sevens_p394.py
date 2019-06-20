ac8ab8ce0bc687ea48a6679c65f4a2a951e4bb08
George Merrill <gm.webdev0@gmail.com>
2019-06-20 11:07:50 -0400

    Create mnist_sevens_p394.py

---
+++ Chapter_12/mnist_sevens_p394.py
@@ -0,0 +1,24 @@
+#code is from 'Python Maching Learning' by Raschka and Mirialili
+#################################################################################
+import os
+import struct
+import numpy as np
+from load_mnist import load_mnist
+import matplotlib.pyplot as plt
+
+X_train, y_train = load_mnist('mnist', kind='train')
+# print('Rows: %d, columns: %d' % (X_train.shape[0], X_train.shape[1]))
+X_test, y_test = load_mnist('mnist', kind='t10k')
+# print('Rows: %d, columns: %d' % (X_test.shape[0], X_test.shape[1]))
+
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True)
+ax =  ax.flatten()
+for i in range(25):
+    img = X_train[y_train == 7][i].reshape(28,28)
+    ax[i].imshow(img, cmap='Greys')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+
