6f267da0179edfbc6c28605c5471b02682adb937
George Merrill <gm.webdev0@gmail.com>
2019-07-03 15:48:31 -0400

    Update neuralnet.py

--- Chapter_12/neuralnet.py
+++ Chapter_12/neuralnet.py
@@ -34,7 +34,7 @@ class NeuralNetMLP(object):
         and validation accuracy for each epoch during trainging.
     """
 
-    def __init__(self, n_hidden=30, l2=0.0, epochs=10, eta=0.001, shuffle=True, minibatch_size=1, seed=None):
+    def __init__(self, n_hidden=30, l2=0.0, epochs=100, eta=0.001, shuffle=True, minibatch_size=1, seed=None):
         self.random = np.random.RandomState(seed)
         self.n_hidden = n_hidden
         self.l2 = l2
@@ -58,11 +58,11 @@ class NeuralNetMLP(object):
         onehot = np.zeros((n_classes, y.shape[0]))
         for idx, val in enumerate(y.astype(int)):
             onehot[val, idx] = 1.
-            return onehot.T
+        return onehot.T
 
     def _sigmoid(self, z):
         """Compute logistic function (sigmoid)"""
-        return 1.0 / (1.0 + np.exp(-np.clip(z, -250, 250)))
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
 
     def _forward(self, X):
         """Compute forward propagation step"""
@@ -102,7 +102,7 @@ class NeuralNetMLP(object):
         """
         L2_term = (self.l2 * (np.sum(self.w_h ** 2.) + np.sum(self.w_out ** 2.)))
         term1 = -y_enc * (np.log(output))
-        term2 = (1.0 - y_enc) * np.log(1.0 - output)
+        term2 = (1. - y_enc) * np.log(1. - output)
         cost = np.sum(term1 - term2) + L2_term
         return cost
 
@@ -142,7 +142,7 @@ class NeuralNetMLP(object):
         ------------------
         self
         """
-        n_output = np.unique(y_train).shape[0] # number of class labels
+        n_output = np.unique(y_train).shape[0]  # number of class labels
         n_features = X_train.shape[1]
 
         ########################
@@ -153,16 +153,21 @@ class NeuralNetMLP(object):
         self.b_h = np.zeros(self.n_hidden)
         self.w_h = self.random.normal(loc=0.0, scale=0.1, size=(n_features, self.n_hidden))
         # weights for hidden -> output
-        self.b_out = np.zeros(self.n_output)
-        self.w_out = self.random.normal(loc=0.0, scale=0.1, size=(self.n_hidden, self.n_output))
+        self.b_out = np.zeros(n_output)
+        self.w_out = self.random.normal(loc=0.0, scale=0.1, size=(self.n_hidden, n_output))
         epoch_strlen = len(str(self.epochs)) # for progr. format.
-        self.eval_ = {'cost' : [], 'train_acc' : [], 'valid_acc' : []}
+        self.eval_ = {'cost': [], 'train_acc': [], 'valid_acc': []}
         y_train_enc = self._onehot(y_train, n_output)
 
+        # iterate over training epochs
         for i in range(self.epochs):
+
+            # iterate over minibatches
             indices = np.arange(X_train.shape[0])
+
             if self.shuffle:
                 self.random.shuffle(indices)
+
             for start_idx in range(0, indices.shape[0] - self.minibatch_size + 1, self.minibatch_size):
                 batch_idx = indices[start_idx:start_idx + self.minibatch_size]
 
@@ -176,8 +181,8 @@ class NeuralNetMLP(object):
                 # [n_samples, n_classlabels]
                 sigma_out = a_out - y_train_enc[batch_idx]
 
-                #[n_samples, n_hidden]
-                sigmoid_derivative_h = a_h * ( 1.0 - a_h)
+                # [n_samples, n_hidden]
+                sigmoid_derivative_h = a_h * (1. - a_h)
 
                 # [n_samples, n_classlabels] dot [n_classlabels, n_hidden]
                 # -> [n_samples, n_hidden]
@@ -194,37 +199,41 @@ class NeuralNetMLP(object):
                 grad_b_out = np.sum(sigma_out, axis=0)
 
                 #Regularization and weight updates
-                delta_w_h = (grad_w_h * self.l2 * self.w_h)
+                delta_w_h = (grad_w_h + self.l2*self.w_h)
                 delta_b_h = grad_b_h # bias is not regularized
                 self.w_h -= self.eta * delta_w_h
                 self.b_h -= self.eta * delta_b_h
 
-                dela_w_out = (grad_w_out * self.l2 * self.w_out)
-                delta_b_out = grad_b_out # bias is not regularized
+                dela_w_out = (grad_w_out + self.l2*self.w_out)
+                delta_b_out = grad_b_out  # bias is not regularized
                 self.w_out -= self.eta * dela_w_out
                 self.b_out -= self.eta * delta_b_out
 
-                ########################
-                # Evaluation
-                ########################
+            ########################
+            # Evaluation
+            ########################
 
-                # Evaluation after each epoch during training
-                z_h, a_h, z_out, a_out = self._forward(X_train)
+            # Evaluation after each epoch during training
+            z_h, a_h, z_out, a_out = self._forward(X_train)
 
-                cost = self._compute_cost(y_enc=y_train_enc, output=a_out)
+            cost = self._compute_cost(y_enc=y_train_enc, output=a_out)
 
-                y_train_pred = self.predict(X_train)
-                y_valid_pred = self.predict(X_valid)
+            y_train_pred = self.predict(X_train)
+            y_valid_pred = self.predict(X_valid)
 
-                train_acc = ((np.sum(y_train == y_train_pred)).astype(np.float) / X_train.shape[0])
-                valid_acc = ((np.sum(y_valid == y_valid_pred)).astype(np.float) / X_valid.shape[0])
+            train_acc = ((np.sum(y_train == y_train_pred)).astype(np.float) /
+                         X_train.shape[0])
+            valid_acc = ((np.sum(y_valid == y_valid_pred)).astype(np.float) /
+                         X_valid.shape[0])
 
-                sys.stderr.write('\r%0*d/%d | Cost:%.2f ' '| Train/Valid Acc: %.2f%%/%.2f%% ' %
-                                 (epoch_strlen, i+1, self.epochs, cost, train_acc*100, valid_acc*100))
-                sys.stderr.flush()
-                self.eval_['cost'].append(cost)
-                self.eval_['train_acc'].append(train_acc)
-                self.eval_['valid_acc'].append(valid_acc)
+            sys.stderr.write('\r%0*d/%d | Cost: %.2f '
+                             '| Train/Valid Acc.: %.2f%%/%.2f%% '
+                             %
+                             (epoch_strlen, i+1, self.epochs, cost, train_acc*100, valid_acc*100))
+            sys.stderr.flush()
+            self.eval_['cost'].append(cost)
+            self.eval_['train_acc'].append(train_acc)
+            self.eval_['valid_acc'].append(valid_acc)
 
-            return self
+        return self
 
